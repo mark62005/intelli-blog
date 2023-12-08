411d19d919a9b64060dbd97ecc9c1d0c56c9f8d4
Mark Wong <markwong.digital@gmail.com>
2023-12-08 06:35:12 -0800

    Create navbar

---
+++ src/config/index.ts
@@ -0,0 +1,72 @@
+export const NAV_LINKS = [
+	{
+		label: "Home",
+		href: "/",
+		variant: "ghost" as const,
+	},
+	{
+		label: "Trending",
+		href: "/trending",
+		variant: "ghost" as const,
+	},
+	{
+		label: "About",
+		href: "/about",
+		variant: "ghost" as const,
+	},
+	{
+		label: "Sign In",
+		href: "/sign-in",
+		variant: "ghost" as const,
+	},
+	{
+		label: "Become a Member",
+		href: "/sign-up",
+		variant: "secondary" as const,
+	},
+];
+
+export const SOCIAL_LINKS = [
+	{
+		href: "https://twitter.com",
+		image: {
+			src: "/social_twitter.png",
+			alt: "Logo of X",
+		},
+	},
+	{
+		href: "https://facebook.com",
+		image: {
+			src: "/social_facebook.png",
+			alt: "Logo of Meta",
+		},
+	},
+	{
+		href: "https://instagram.com",
+		image: {
+			src: "/social_instagram.png",
+			alt: "Logo of Instagram",
+		},
+	},
+	{
+		href: "https://google.com",
+		image: {
+			src: "/social_google.png",
+			alt: "Logo of Google",
+		},
+	},
+	{
+		href: "https://pinterest.com",
+		image: {
+			src: "/social_pinterest.png",
+			alt: "Logo of Pinterest",
+		},
+	},
+	{
+		href: "https://discord.com",
+		image: {
+			src: "/social_discord.png",
+			alt: "Logo of Discord",
+		},
+	},
+];
